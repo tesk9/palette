61967affefb849994f006c4ce1107830073e612f
Tessa Kelly <tessakkelly@gmail.com>
2018-09-21 17:29:21 -0700

    Test against black and white expected contrasts

--- tests/PaletteSpec.elm
+++ tests/PaletteSpec.elm
@@ -1,4 +1,7 @@
-module PaletteSpec exposing (black, luminanceSuite, white)
+module PaletteSpec exposing
+    ( contrastSuite
+    , luminanceSuite
+    )
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
@@ -29,6 +32,29 @@ luminanceSuite =
                 black
                     |> Palette.luminance
                     |> floatEqual 0
+        ]
+
+
+contrastSuite : Test
+contrastSuite =
+    describe "contrast"
+        [ describe "black and white"
+            [ test "contrast black white == contrast white black" <|
+                \_ ->
+                    Expect.equal (Palette.contrast black white) (Palette.contrast white black)
+            , test "contrast black white" <|
+                \_ ->
+                    Palette.contrast black white
+                        |> floatEqual 21
+            , test "contrast white white" <|
+                \_ ->
+                    Palette.contrast white white
+                        |> floatEqual 1
+            , test "contrast black black" <|
+                \_ ->
+                    Palette.contrast black black
+                        |> floatEqual 1
+            ]
         ]
 
 
