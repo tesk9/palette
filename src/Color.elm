728fcc99c2f9e2038db62a97d3e882a91def9669
Tessa Kelly <tessakkelly@gmail.com>
2018-09-29 13:35:54 -0700

    Adds an image

--- src/Color.elm
+++ src/Color.elm
@@ -22,6 +22,9 @@ You may be intuitively familiar with HSL color modeling if you've worked with a
 color wheel before. It also may be a great place to start working with color if
 you enjoyed playing with unit circles and polar coordinates in trigonometry.
 
+![Representation of HSL values on a cylinder](https://upload.wikimedia.org/wikipedia/commons/6/6b/HSL_color_solid_cylinder_saturation_gray.png)
+(Image can be seen in context on the [HSL and HSV arcticle on Wikipedia](https://en.wikipedia.org/wiki/HSL_and_HSV). By HSL\_color\_solid\_cylinder.png: SharkDderivative work: SharkD  Talk - HSL\_color\_solid\_cylinder.png, CC BY-SA 3.0, <https://commons.wikimedia.org/w/index.php?curid=9801661>)
+
 HSL models hues as values on a circle. We can pick a hue by providing a degree.
 We start at red -- meaning that we can get to red by saying that our hue is 0 degrees or
 by saying that our hue is at 360 degrees. Green is at 90, teal is at 180, and
