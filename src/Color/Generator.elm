ffab617bba097124d250fe6e7f69696e73affb45
Tessa Kelly <tessakkelly@gmail.com>
2018-09-29 16:14:49 -0700

    Add naive complementary color generator

--- src/Color/Generator.elm
+++ src/Color/Generator.elm
@@ -11,13 +11,14 @@ import Color exposing (Color)
 
 {-| Find the color opposite the color you pass in on the color wheel.
 
-E.g., if you pass in a reddish color, you should expect to get back a greenish color.
+E.g., if you pass in a reddish color, you should expect to get back a tealish color.
 
 -}
 complementary : Color -> Color
 complementary color =
-    --TODO: implement!
-    color
+    Color.toHSL color
+        |> (\( h, s, l ) -> ( h + 180, s, l ))
+        |> Color.fromHSL
 
 
 {-| Convert the color you pass in to a grayscale version. Essentially this uses the
