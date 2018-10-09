1dc4bde2509b53471748a90c2ea6f1bfe1b790cd
Tessa Kelly <tessakkelly@gmail.com>
2018-10-08 18:58:14 -0700

    Adds example links to Ellie

--- src/Color/Blend.elm
+++ src/Color/Blend.elm
@@ -1,6 +1,6 @@
 module Color.Blend exposing (add, subtract, multiply, divide)
 
-{-|
+{-| Operations that combine colors.
 
 @docs add, subtract, multiply, divide
 
@@ -11,7 +11,9 @@ import Color exposing (Color)
 
 {-| Blends two colors together by adding the values in each channel.
 
-That is, rgb(10, 20, 30) + rgb(10, 10, 10) = rgb(20, 30, 40).
+That is, `rgb(10, 20, 30) + rgb(10, 10, 10) = rgb(20, 30, 40)`.
+
+Play with an example in Ellie here: <https://ellie-app.com/3yLdpDs9NBya1>.
 
 -}
 add : Color -> Color -> Color
@@ -33,7 +35,9 @@ add a c =
 {-| Blends two colors together by subtracting the second color's channel values from
 the first color's channel values.
 
-That is, rgb(10, 20, 30) - rgb(10, 10, 10) = rgb(0, 10, 20).
+That is, `rgb(10, 20, 30) - rgb(10, 10, 10) = rgb(0, 10, 20)`.
+
+Play with an example in Ellie here: <https://ellie-app.com/3yLftQKkL6Ga1>.
 
 -}
 subtract : Color -> Color -> Color
@@ -56,7 +60,9 @@ subtract a c =
 
 Any color multiplied by black will result in black.
 Any color multiplied by white will result in the color.
-rgb(255, 0, 0) will keep reds and remove any greens and blues.
+`rgb(255, 0, 0)` will keep reds and remove any greens and blues.
+
+Play with an example in Ellie here: <https://ellie-app.com/3yLgG6JQCgHa1>.
 
 -}
 multiply : Color -> Color -> Color
@@ -78,6 +84,8 @@ multiply a c =
 {-| Blend two colors together.
 
 Use this function to strip out tones & change them to white.
+
+Play with an example in Ellie here: <https://ellie-app.com/3yLhRLkJPwTa1>
 
 -}
 divide : Color -> Color -> Color
