60c9b3534bd7ca83059b99d2712c47d317465268
Tessa Kelly <tessakkelly@gmail.com>
2018-10-15 18:50:37 -0700

    Begins adding and position the slider on the page

--- examples/src/ColorPicker.elm
+++ examples/src/ColorPicker.elm
@@ -3,7 +3,8 @@ module ColorPicker exposing (Model, Msg, init, update, view)
 import Color exposing (Color)
 import Color.Generator exposing (adjustLightness, adjustSaturation, rotate)
 import Html exposing (Html)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (attribute, id, style)
+import Html.Events
 
 
 type Model
@@ -20,19 +21,46 @@ type alias Msg =
 
 
 view : Model -> Html Msg
-view (Model color) =
+view model =
     Html.div
         []
-        [ viewHueSelector color
+        [ viewHueSelector model
         ]
 
 
-viewHueSelector : Color -> Html Msg
-viewHueSelector color =
-    List.range 1 360
-        |> List.map (\rotation -> rotate (toFloat rotation) color)
-        |> List.map viewHueSlice
-        |> Html.div []
+viewHueSelector : Model -> Html Msg
+viewHueSelector (Model selectedColor) =
+    let
+        ( currentHue, _, _ ) =
+            Color.toHSL selectedColor
+
+        labelId =
+            "hue-selector"
+
+        slider =
+            Html.div
+                [ attribute "aria-role" "slider"
+                , attribute "aria-valuemin" "1"
+                , attribute "aria-valuemax" "360"
+                , attribute "aria-valuenow" (String.fromFloat currentHue)
+                , attribute "aria-labelledby" labelId
+                , attribute "tabindex" "0"
+                , style "width" "100px"
+                , style "height" "1px"
+                , style "border" "1px solid black"
+                , style "position" "relative"
+                , style "top" (String.fromFloat currentHue ++ "px")
+                ]
+                []
+
+        rainbow =
+            List.range 1 360
+                |> List.map (\hue -> viewHueSlice (Color.fromHSL ( toFloat hue, 100, 50 )))
+    in
+    Html.div []
+        [ Html.div [] (slider :: rainbow)
+        , Html.label [ id labelId ] [ Html.text "Hue Selector" ]
+        ]
 
 
 viewHueSlice : Color -> Html Msg
@@ -40,7 +68,8 @@ viewHueSlice color =
     Html.div
         [ style "width" "100px"
         , style "height" "1px"
-        , style "background-color" (Color.toRGBString color)
+        , style "margin-left" "1px"
+        , style "background-color" (Color.toHSLString color)
         ]
         []
 
