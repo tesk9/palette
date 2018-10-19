d69c097ade5d3065d218825219c29ca40b30ddd6
Tessa Kelly <tessakkelly@gmail.com>
2018-10-18 17:14:17 -0700

    Introduce generator idea to the model

--- examples/src/Preview.elm
+++ examples/src/Preview.elm
@@ -1,56 +1,118 @@
 module Preview exposing (Model, Msg, init, update, view)
 
 import Color exposing (Color)
-import Color.Generator
+import Color.Generator as Generator
 import Comparison
 import Html exposing (Html)
 import Html.Attributes exposing (style)
+import Html.Events
+import Json.Decode
 
 
 type alias Model =
-    {}
+    { generators : List Generator
+    , selectedGenerator : Generator
+    }
+
+
+type Generator
+    = Generator String (Color -> List Color)
+
+
+polychromatic : ( Generator, List Generator )
+polychromatic =
+    ( Generator "Color.Generator.complementary"
+        (normalizeSingularFunction Generator.complementary)
+    , [ Generator "Color.Generator.triadic"
+            (normalizeTupleFunction Generator.triadic)
+
+      --, Generator "Color.Generator.splitComplementary" Generator.splitComplementary
+      , Generator "Color.Generator.square"
+            (normalizeTripleFunction Generator.square)
+
+      --, Generator "Color.Generator.tetratic"
+      --      (normalizeTripleFunction Generator.tetratic)
+      --, Generator "Color.Generator.monochromatic" Generator.monochromatic
+      ]
+    )
+
+
+normalizeSingularFunction : (a -> a) -> a -> List a
+normalizeSingularFunction func color =
+    [ func color ]
+
+
+normalizeTupleFunction : (a -> ( a, a )) -> a -> List a
+normalizeTupleFunction func color =
+    let
+        ( one, two ) =
+            func color
+    in
+    [ one, two ]
+
+
+normalizeTripleFunction : (a -> ( a, a, a )) -> a -> List a
+normalizeTripleFunction func color =
+    let
+        ( one, two, three ) =
+            func color
+    in
+    [ one, two, three ]
 
 
 init : Model
 init =
-    {}
+    { generators = Tuple.first polychromatic :: Tuple.second polychromatic
+    , selectedGenerator = Tuple.first polychromatic
+    }
 
 
 view : Color -> Model -> Html Msg
 view selectedColor model =
+    case model.selectedGenerator of
+        Generator name generate ->
+            Html.div [ style "margin-left" "20px" ]
+                [ Html.h3 [] [ Html.text "Generate additional colors" ]
+                , generatorOptions model
+                , Html.h4 [] [ Html.text name ]
+                , Comparison.viewPalette selectedColor (generate selectedColor)
+                ]
+
+
+generatorOptions : Model -> Html Msg
+generatorOptions model =
     Html.div []
-        [ Html.h3 [] [ Html.text "Palette generators" ]
-        , Html.div [ style "text-align" "center" ]
-            [ viewComplementary selectedColor
-            , Html.text ".complementary"
+        [ Html.label [ Html.Attributes.for "generator-select" ]
+            [ Html.text "Select a generator" ]
+        , Html.select
+            [ Html.Attributes.id "generator-select"
+            , Html.Events.onInput SetGenerator
             ]
-        , Html.div [ style "text-align" "center" ]
-            [ viewTriadic selectedColor
-            , Html.text ".triadic"
-            ]
+            (List.map (generatorOption model.selectedGenerator) model.generators)
         ]
 
 
-viewComplementary : Color -> Html msg
-viewComplementary color =
-    Comparison.viewPalette color [ Color.Generator.complementary color ]
+generatorOption : Generator -> Generator -> Html Msg
+generatorOption (Generator selectedGenerator _) (Generator name generator) =
+    Html.option
+        [ Html.Attributes.value name
+        , Html.Attributes.selected (name == selectedGenerator)
+        ]
+        [ Html.text name ]
 
 
-viewTriadic : Color -> Html msg
-viewTriadic color =
-    let
-        ( one, two ) =
-            Color.Generator.triadic color
-    in
-    Comparison.viewPalette color [ one, two ]
-
-
-type alias Msg =
-    ()
+type Msg
+    = SetGenerator String
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        () ->
-            model
+        SetGenerator generator ->
+            { model
+                | selectedGenerator =
+                    model.generators
+                        |> List.filter (\(Generator name _) -> name == generator)
+                        |> List.head
+                        |> Maybe.withDefault model.selectedGenerator
+            }
