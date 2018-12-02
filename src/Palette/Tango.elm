c0ef800441b6a615333b1d7e7876fe7ec8feb029
Tessa Kelly <tessakkelly@gmail.com>
2018-12-02 15:42:55 -0800

    Adds initial version of the tango palette

---
+++ src/Palette/Tango.elm
@@ -0,0 +1,145 @@
+module Palette.Tango exposing
+    ( butter1, butter2, butter3
+    , orange1, orange2, orange3
+    , chocolate1, chocolate2, chocolate3
+    , chameleon1, chameleon2, chameleon3
+    , skyBlue1, skyBlue2, skyBlue3
+    , plum1, plum2, plum3
+    , scarletRed1, scarletRed2, scarletRed3
+    )
+
+{-|
+
+
+## Butters
+
+@docs butter1, butter2, butter3
+
+
+## Oranges
+
+@docs orange1, orange2, orange3
+
+
+## Chocolates
+
+@docs chocolate1, chocolate2, chocolate3
+
+
+## Chameleons
+
+@docs chameleon1, chameleon2, chameleon3
+
+
+## Sky Blues
+
+@docs skyBlue1, skyBlue2, skyBlue3
+
+
+## Plums
+
+@docs plum1, plum2, plum3
+
+
+## Scarlet Reds
+
+@docs scarletRed1, scarletRed2, scarletRed3
+
+
+## Aluminums
+
+TODO: @docs aluminum1, aluminum2, aluminum3
+
+-}
+
+import Color exposing (Color)
+
+
+butter1 =
+    Color.fromHexString "#fce94f"
+
+
+butter2 =
+    Color.fromHexString "#edd400"
+
+
+butter3 =
+    Color.fromHexString "#c4a000"
+
+
+orange1 =
+    Color.fromHexString "#fcaf3e"
+
+
+orange2 =
+    Color.fromHexString "#f57900"
+
+
+orange3 =
+    Color.fromHexString "#ce5c00"
+
+
+chocolate1 =
+    Color.fromHexString "#e9b96e"
+
+
+chocolate2 =
+    Color.fromHexString "#c17d11"
+
+
+chocolate3 =
+    Color.fromHexString "#8f5902"
+
+
+chameleon1 =
+    Color.fromHexString "#8ae234"
+
+
+chameleon2 =
+    Color.fromHexString "#73d216"
+
+
+chameleon3 =
+    Color.fromHexString "#4e9a06"
+
+
+skyBlue1 =
+    Color.fromHexString "#729fcf"
+
+
+skyBlue2 =
+    Color.fromHexString "#3465a4"
+
+
+skyBlue3 =
+    Color.fromHexString "#204a87"
+
+
+plum1 =
+    Color.fromHexString "#ad7fa8"
+
+
+plum2 =
+    Color.fromHexString "#75507b"
+
+
+plum3 =
+    Color.fromHexString "#5c3566"
+
+
+scarletRed1 =
+    Color.fromHexString "#ef2929"
+
+
+scarletRed2 =
+    Color.fromHexString "#cc0000"
+
+
+scarletRed3 =
+    Color.fromHexString "#a40000"
+
+
+
+--TODO: organize aluminum colors
+--Aluminum
+--    eeeeec d3d7cf babdb6 888a85 555753 2e3436
