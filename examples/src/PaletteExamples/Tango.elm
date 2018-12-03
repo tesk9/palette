aa8bb691185434e7661ec4806f19e78845e2e2a1
Tessa Kelly <tessakkelly@gmail.com>
2018-12-02 16:53:57 -0800

    Show the rgb values for the tango colors

--- examples/src/PaletteExamples/Tango.elm
+++ examples/src/PaletteExamples/Tango.elm
@@ -1,5 +1,6 @@
 module PaletteExamples.Tango exposing (examples)
 
+import Color
 import Comparison
 import ExampleHelpers as Example
 import Html exposing (Html)
@@ -16,99 +17,59 @@ examples =
                 , ( butter2, "butter2" )
                 , ( butter3, "butter3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Oranges" ]
             , Example.list
                 [ ( orange1, "orange1" )
                 , ( orange2, "orange2" )
                 , ( orange3, "orange3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Chocolates" ]
             , Example.list
                 [ ( chocolate1, "chocolate1" )
                 , ( chocolate2, "chocolate2" )
                 , ( chocolate3, "chocolate3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Chameleons" ]
             , Example.list
                 [ ( chameleon1, "chameleon1" )
                 , ( chameleon2, "chameleon2" )
                 , ( chameleon3, "chameleon3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Sky Blues" ]
             , Example.list
                 [ ( skyBlue1, "skyBlue1" )
                 , ( skyBlue2, "skyBlue2" )
                 , ( skyBlue3, "skyBlue3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Plums" ]
             , Example.list
                 [ ( plum1, "plum1" )
                 , ( plum2, "plum2" )
                 , ( plum3, "plum3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Scarlet Reds" ]
             , Example.list
                 [ ( scarletRed1, "scarletRed1" )
                 , ( scarletRed2, "scarletRed2" )
                 , ( scarletRed3, "scarletRed3" )
                 ]
-                (\( c, n ) ->
-                    case c of
-                        Ok color ->
-                            Comparison.viewWithName ( color, n )
-
-                        Err err ->
-                            Html.text "OOOPs"
-                )
+                viewColor
             , Html.h4 [] [ Html.text "Aluminums" ]
             , Html.text "TODO"
             ]
         )
+
+
+viewColor ( c, n ) =
+    case c of
+        Ok color ->
+            Comparison.viewWithName ( color, Color.toRGBString color )
+
+        Err err ->
+            Html.text "OOOPs"
