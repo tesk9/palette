5b1a08bc296e4c9fa0d54df0939c9df33c5ad057
Tessa Kelly <tessakkelly@gmail.com>
2019-01-20 10:47:04 -0800

    Adds additional generation examples to the styleguide

--- examples/src/PaletteExamples/Cubehelix.elm
+++ examples/src/PaletteExamples/Cubehelix.elm
@@ -4,6 +4,7 @@ import Color
 import Comparison
 import ExampleHelpers as Example
 import Html exposing (Html)
+import Html.Attributes exposing (style)
 import Palette.Cubehelix as Cubehelix exposing (defaultConfig)
 
 
@@ -16,12 +17,24 @@ examples =
                 [ ( Cubehelix.generate defaultConfig
                   , "Cubehelix.generate Cubehelix.defaultConfig"
                   )
+                , ( Cubehelix.generate { defaultConfig | start = 2, saturation = 1.0 }
+                  , "Cubehelix.generate { defaultConfig | start = 2, saturation = 1.0 }"
+                  )
+                , ( Cubehelix.generate { defaultConfig | start = 3, saturation = 1.0 }
+                  , "Cubehelix.generate { defaultConfig | start = 3, saturation = 1.0 }"
+                  )
                 ]
                 viewExample
             , Html.h4 [] [ Html.text "Grayscale" ]
             , Example.list
                 [ ( Cubehelix.generate { defaultConfig | saturation = 0 }
                   , "Cubehelix.generate { defaultConfig | saturation = 0 }"
+                  )
+                , ( Cubehelix.generate { defaultConfig | saturation = 0, start = 2 }
+                  , "Cubehelix.generate { defaultConfig | saturation = 0, start = 2 }"
+                  )
+                , ( Cubehelix.generate { defaultConfig | saturation = 0, start = 3 }
+                  , "Cubehelix.generate { defaultConfig | saturation = 0, start = 3 }"
                   )
                 ]
                 viewExample
@@ -31,7 +44,7 @@ examples =
 
 viewExample : ( List Color.Color, String ) -> Html msg
 viewExample ( colors, description ) =
-    Html.div []
+    Html.div [ style "display" "flex", style "align-items" "center" ]
         [ Comparison.viewSpectrum colors
         , Html.code [] [ Html.text description ]
         ]
