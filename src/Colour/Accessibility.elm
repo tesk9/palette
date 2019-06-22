9745fd1092f7d2880996bd2624e0700d926ccf2e
Tessa Kelly <tessakkelly@gmail.com>
2019-06-21 19:38:38 -0700

    :skull: remove outdated docs

--- src/Colour/Accessibility.elm
+++ src/Colour/Accessibility.elm
@@ -79,21 +79,7 @@ type WCAGLevel
     | AAA_
 
 
-{-| For a given WCAG level, calculate whether two colors have enough contrast
-with each other to be used together (e.g., as a background and text color combination).
-
-To meet AA level sufficiently, [follow these standards](https://www.w3.org/WAI/WCAG21/quickref/?versions=2.0&showtechniques=143%2C146#contrast-minimum).
-To meet AAA level sufficiently, [follow these standards](https://www.w3.org/WAI/WCAG21/quickref/?versions=2.0&showtechniques=143%2C146#contrast-enhanced).
-
-TODO (consider this a headsup on likely API changes!):
-
-  - Use named fontweights rather than numbers
-  - Wrap fontsize with some constructors
-  - Cassowary constraint solving..?
-
-See an example here: <https://ellie-app.com/3CgJZNMyxw3a1>.
-
--}
+{-| -}
 sufficientContrast : WCAGLevel -> { fontSize : Float, fontWeight : Int } -> OpaqueColor -> OpaqueColor -> Bool
 sufficientContrast wcagLevel { fontSize, fontWeight } color1 color2 =
     let
@@ -117,9 +103,6 @@ sufficientContrast wcagLevel { fontSize, fontWeight } color1 color2 =
 
 
 {-| Calculate the contrast between two colors.
-
-See an example here: <https://ellie-app.com/3CgJZNMyxw3a1>.
-
 -}
 contrast : OpaqueColor -> OpaqueColor -> Float
 contrast color1 color2 =
